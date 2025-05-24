c039f4dfa44d2a62dc78304a74bd60698700c5c1
7yzx <2804006356@qq.com>
2025-05-25 01:25:42 +0800

    copy a depth raster

--- scripts/preprocess_replica.py
+++ scripts/preprocess_replica.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from glob import glob
 from scipy.spatial.transform import Rotation as R
-
+from pathlib import Path
 
 def to_se3_vec(pose):
     quat = R.from_matrix(pose[:3, :3]).as_quat()
@@ -11,19 +11,31 @@ def to_se3_vec(pose):
 
 parent = os.path.dirname(__file__)
 seqs = sorted(glob('data/Replica/room*')) + sorted(glob('data/Replica/office*'))
+seqs = ["/home/farsee2/YZX_code/datasets/Replica/office0/"]
+result_data = "./results"
 for seq in seqs:
     print(seq)
-
-    os.system(f'rm -rf {seq}/colors')
-    os.system(f'rm -rf {seq}/depths')
-    os.makedirs(f'{seq}/colors', exist_ok=True)
-    os.makedirs(f'{seq}/depths', exist_ok=True)
+    base_name = Path(seq).name
+    result_path = f'{result_data}/{base_name}'
+    os.makedirs(result_path, exist_ok=True)
+    os.system(f'rm -rf {result_path}/colors')
+    os.system(f'rm -rf {result_path}/depths')
+    os.makedirs(f'{result_path}/colors', exist_ok=True)
+    os.makedirs(f'{result_path}/depths', exist_ok=True)
     
     for color, depth in zip(glob(f'{seq}/results/frame*'), glob(f'{seq}/results/depth*')):
-        # print(color)
-        os.symlink(f'../results/{os.path.basename(color)}', color.replace('results', 'colors'))
-        os.symlink(f'../results/{os.path.basename(depth)}', depth.replace('results', 'depths'))
+        color_name = os.path.basename(color)
+        depth_name = os.path.basename(depth)
+
+        color_link = os.path.join(result_path, 'colors', color_name)
+        depth_link = os.path.join(result_path, 'depths', depth_name)
+
+        color_src = os.path.abspath(color)
+        depth_src = os.path.abspath(depth)
+
+        os.symlink(color_src, color_link)
+        os.symlink(depth_src, depth_link)
 
     traj = np.loadtxt(f"{seq}/traj.txt").reshape(-1, 4, 4)
     traj_tum = [np.hstack(([i], to_se3_vec(p))) for i, p in enumerate(traj)]
-    np.savetxt(f'{seq}/traj_tum.txt', traj_tum)
+    np.savetxt(f'{result_path}/traj_tum.txt', traj_tum)
